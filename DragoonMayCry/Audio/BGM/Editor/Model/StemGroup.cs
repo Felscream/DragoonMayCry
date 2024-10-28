bd0d44773b030e666ba02838bc0b25f5051f4dec
Felscream <xblackfieldx@gmail.com>
2024-10-28 21:00:17 +0100

    model changes

--- DragoonMayCry/Audio/BGM/Editor/Model/StemGroup.cs
+++ DragoonMayCry/Audio/BGM/Editor/Model/StemGroup.cs
@@ -5,6 +5,6 @@ namespace DragoonMayCry.Audio.BGM.Editor.Model;
 public class StemGroup
 {
     public string Name { get; set; } = "";
-    public List<Stem> Tracks { get; set; } = [];
-    public StemGroup? NextTrackGroup { get; set; }
+    public List<Stem> Stems { get; set; } = [];
+    public StemGroup? NextStemGroup { get; set; }
 }
